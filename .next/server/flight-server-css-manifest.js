639e781d781301d71e2a34c2be64f798087ff9b2
Sagar Borude <87903581+MrSagarRB@users.noreply.github.com>
2023-02-23 19:01:23 +0530

    ui

--- .next/server/flight-server-css-manifest.js
+++ .next/server/flight-server-css-manifest.js
@@ -1,10 +1,3 @@
 self.__RSC_CSS_MANIFEST={
-  "__entry_css_mods__": {
-    "/Users/atomostechnologies/Desktop/IMP Projects/show-case/next-app/app/page": [
-      "/Users/atomostechnologies/Desktop/IMP Projects/show-case/next-app/styles/globals.css"
-    ]
-  },
-  "/Users/atomostechnologies/Desktop/IMP Projects/show-case/next-app/app/layout.tsx": [
-    "/Users/atomostechnologies/Desktop/IMP Projects/show-case/next-app/styles/globals.css"
-  ]
+  "__entry_css_mods__": {}
 }
